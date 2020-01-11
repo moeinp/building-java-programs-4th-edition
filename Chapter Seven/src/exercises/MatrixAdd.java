9a2ea1d9fa1cea3e027b9da63423aedcb9417051
Moein Pardakhtim <moeined@gmail.com>
2020-01-11 14:31:02 -0800

    finish chapter 7

--- Chapter Seven/src/exercises/MatrixAdd.java
+++ Chapter Seven/src/exercises/MatrixAdd.java
@@ -6,7 +6,11 @@ public class MatrixAdd {
 
 	}
 	public static void matrixAdd(int[] arr1, int[] arr2) {
-		
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr2[i].length; j++) {
+				
+			}
+		}
 	}
 
 }
